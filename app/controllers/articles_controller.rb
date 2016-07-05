857fc950fd8eee9b6a6663c51a7d3346cbaf35d3
Christopher <Button@Christophers-MacBook-Air.local>
2016-07-06 08:17:08 +0900

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,8 @@ class ArticlesController < ApplicationController
     def show
         # should I keep the "params hash" (which is actually a method which returns a hash)?
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Blogger::Application.routes.draw do
   # so we have  'resource' and it is called ':articles'
   # and router should expect requests to follow the RESTful mod of web interaction
   root to: 'articles#index'
-  resources :articles
+  resources :articles do
+      resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
